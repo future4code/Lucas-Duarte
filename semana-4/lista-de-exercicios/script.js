d1ea3fdb82bb198d507b0e07e1189fe5aa01046c
Lucas  Duarte <lucasduarte@MacBook-Pro-de-Lucas.local>
2020-08-07 14:08:25 -0300

    Exercícios de interpretação de código

---
+++ semana-4/lista-de-exercicios/script.js
@@ -0,0 +1,24 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+/* 1. Essa função 1)recebe um valor, 2)pede via prompt um novo valor ao usuário, que será a cotação do dólar, 3)retorna o resultado da multiplicação do valor recebido e do valor digitado no prompt.
+
+IMPRESSO NO CONSOLE: "R$[100 x cotação do dólar inserida no prompt]"*/
+
+/* 2. A função recebe 2 parâmetros e inicia declarando uma variável valorAposInvestimento. É a usada a condicional switch para oferecer um valor diferente para essa variável de acordo com o valor da string do primeiro parâmetro. O segundo parâmetro é multiplicado por um denominador diferente, dependendo do valor do primeiro.
+
+IMPRESSO NO CONSOLE1: 165
+IMPRESSO NO CONSOLE2: TIPO DE INVESTIMENTO INFORMADO INCORRETO
+*/
+
+/* 3. São 3 arrays. O primeiro com vários valores, o segundo e o terceiro vazios. É feita uma iteração sobre o primeiro array, com uma condicional: os números pares serão inseridos no segundo array, e os ímpares no terceiro.
+
+IMPRESSO NO CONSOLE1: Quantidade total de números 14
+IMPRESSO NO CONSOLE2: [quantidade de números pares]
+IMPRESSO NO CONSOLE3: [quantidade de números ímpares]
+*/
+
+/* 4. A iteração sobre o array determina que numero1 será o menor número do array e numero 2 será o maior número do array.
+
+IMPRESSO NO CONSOLE1: -10
+IMPRESSO NO CONSOLE2: 1590
+*/
