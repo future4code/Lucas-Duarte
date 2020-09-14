e0dc52e7263faad357aded41144e9869c3bb8ee2
Lucas  Duarte <dolucasduarte@gmail.com>
2020-09-14 16:21:46 -0300

    LabeX - Fluxograma + Wireframes

---
+++ semana-10/projeto-labex/labex/src/components/ListTripsPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TripsListPage() {
+    return (
+    <div>
+      formulário de aplicação
+    </div>
+    );
+  }
---
+++ semana-10/projeto-labex/labex/src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginPage() {
+    return (
+    <div>
+      pagina de login
+    </div>
+    );
+  }
